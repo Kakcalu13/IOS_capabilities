78c81ad35907692a43bdd0d7eddb129ca0ebde50
kakcalu13 <kevin.a.araujo@gmail.com>
2024-06-30 17:52:23 -0400

    added mobile again

---
+++ mobile/mobile/mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileApp.swift
+//  mobile
+//
+//  Created by KO on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
